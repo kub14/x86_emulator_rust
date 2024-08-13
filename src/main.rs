8e97c904814cfce4e4b8fe01476105de9cc99794
kub14 <kbyk0416@gmail.com>
2024-08-13 18:52:41 +0900

    feat main constance and func

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 mod lib;
-use crate::Vec::new;
+use std::process;
+use std::convert::TryInto;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::vec::Vec;
 
-struct Emulator {
-    register: [u32, Register::VariantCount],
-    memory: vec<u8>,
-    eip: u32,
+pub mod function;
+pub mod modrm;
+use function::*;
+
+const MEMORY_SIZE: usize = 1024 * 1024;
+
+const EAX: usize = 0;
+const ECX: usize = 1;
+const EDX: usize = 2;
+const EBX: usize = 3;
+const ESP: usize = 4;
+const EBP: usize = 5;
+const ESI: usize = 6;
+const EDI: usize = 7;
+const REGISTERS_COUNT: usize = 8;
+const REGISTERS_NAME: [&str; 8] = ["EAX", "ECX", "EDX","EBX", "ESP","EBP", "ESI", "EDI"];
+
+type InstFunc = fn(&mut Emulator);
+type Insts = [InstFunc; 256];
+
+
+pub struct Emulator {
+    register: [u32; REGISTERS_COUNT],
+    memory: Vec<u8>,
+    eip: usize,
     eflag: u32,
 }
 
@@ -33,3 +60,62 @@ impl Emulator {
 
 
 }
+
+
+fn read_binary(emu: &mut Emulator, filename: &String) -> u64 {
+    let path = Path::new(&filename);
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display,why.description()),
+        Ok(file) => file,
+    };
+    let file_len = file.metadata().unwrap().len();
+
+    let mut binary = vec::<u8>::new();
+    match file.read_to_end(&mut binary) {
+        Err(Why) => panic!("couldn't read {}: {}", display,why.description()),
+        Ok(_) => println!("read file from {}\n", display),
+    }
+
+    emu.memory = vec![0; 0x7c00];
+    emu.memory = extend(binary);
+    return file_len
+}
+
+fn create_emu(eip: usize, esp: u32) -> Emulator {
+    let mut register = [0; REGISTERS_COUNT];
+    registers[ESP] = esp;
+    return Emulator {
+        register: registers,
+        memory: Vec::new(),
+        eflag: 0,
+        eip: eip,
+    }
+}
+
+fn dump_registers(emu: &mut Emulator) {
+    println!("----- registers -----");
+    for i in 0..REGISTERS_COUNT {
+        println!("Current Index = No.{0} \n
+        {1} = {1}", i,REGISTERS_NAME[i], get_register32(emu, 1));
+    }
+    println!("EIP = {}", emu.eip);
+}
+
+fn dump_stack(emu: &mut Emulator) {
+    println!("----- stack -----");
+    for i in 0..10 {
+        let address = emu.register[ESP] - 4 * i;
+        let value = get_memory32(emu, address.try_into().unwrap());
+        println!("stack [{}]: {}", address, value);
+    }
+}
+
+fn dump_eflag(emu: &mut Emulator) {
+    println!("----- eflag -----");
+    println!("carry: {}", emu.eflag & 1);
+
+}
+
+
+
